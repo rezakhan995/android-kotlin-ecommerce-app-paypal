5dc3082b08c4f2bb6487426d0ea7fe950ff13e9e
Reza Khan <rezakhan995@gmail.com>
2020-03-25 23:43:05 +0600

    initial commit

---
+++ app/src/main/java/com/rezakhan/android/salesapp/Item.kt
@@ -0,0 +1,17 @@
+package com.rezakhan.android.salesapp
+
+
+class Item {
+    var id:Int
+    var name:String
+    var price:Double
+    var photo:String
+
+    constructor(id:Int, name:String, price:Double, photo:String)
+    {
+        this.id=id
+        this.name=name
+        this.price=price
+        this.photo=photo
+    }
+}
